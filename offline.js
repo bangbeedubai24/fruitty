5f1454add33884d97ee501d0369bdf48a6b74063
bangbeedubai24 <52026805+bangbeedubai24@users.noreply.github.com>
2019-07-11 11:26:11 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1562818314,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ap-sheet0.png",
+		"images/sb-sheet0.png",
+		"images/k-sheet0.png",
+		"images/tm-sheet0.png",
+		"images/tg-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/kd-sheet0.png",
+		"images/box-sheet0.png",
+		"images/v-sheet0.png",
+		"images/cl-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/gmo-sheet0.png",
+		"images/po-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
